396616c5faf49b0beed2f99b948ee5271ea25507
Nao k <light.kart92@gmail.com>
2023-07-26 21:31:44 +0900

    refactoring posting state

--- pages/board/create.tsx
+++ pages/board/create.tsx
@@ -1,41 +1,75 @@
 import { useRouter } from 'next/router';
-import { FormControl, Input, FormLabel, Stack, Button } from '@chakra-ui/react';
+import {
+  FormControl,
+  Input,
+  FormLabel,
+  Stack,
+  Button,
+  Select,
+} from '@chakra-ui/react';
 import { useMutatePosting } from '../../hooks/useMutatePosting';
 import { useState } from 'react';
+import { Layout } from '../../components/Layout';
+import { gameTitleLabel } from './game_title/gameTitle';
 
 const CreatePosting = () => {
   const { createPostingMutation } = useMutatePosting();
+  const [newPosting, setPosting] = useState({
+    title: '',
+    content: '',
+    gameTitle: 'any title',
+  });
   const router = useRouter();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+
   const addPosting = () => {
-    createPostingMutation.mutate({ title, content });
-    setTitle('');
-    setContent('');
+    createPostingMutation.mutate({ ...newPosting });
+    setPosting({ title: '', content: '', gameTitle: 'any title' });
     router.push(`/board`);
   };
+
   return (
-    <Stack spacing={4}>
-      <FormControl>
-        <FormLabel>Posting Title</FormLabel>
-        <Input
-          type='text'
-          placeholder='Enter a posting title'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Posting Details</FormLabel>
-        <Input
-          type='text'
-          placeholder='Enter a Details'
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-      </FormControl>
-      <Button onClick={() => addPosting()}>Create Posting</Button>
-    </Stack>
+    <Layout title='Create Posting'>
+      <Stack spacing={4}>
+        <FormControl>
+          <FormLabel>Posting Title</FormLabel>
+          <Input
+            type='text'
+            placeholder='Enter a posting title'
+            value={newPosting.title}
+            onChange={(e) =>
+              setPosting({ ...newPosting, title: e.target.value })
+            }
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Posting Details</FormLabel>
+          <Input
+            type='text'
+            placeholder='Enter a Details'
+            value={newPosting.content}
+            onChange={(e) =>
+              setPosting({ ...newPosting, content: e.target.value })
+            }
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Game Title</FormLabel>
+          <Select
+            value={newPosting.gameTitle}
+            onChange={(e) =>
+              setPosting({ ...newPosting, gameTitle: e.target.value })
+            }
+          >
+            {gameTitleLabel.map((data, index) => (
+              <option key={index} value={data.value}>
+                {data.gameTitle}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+        <Button onClick={() => addPosting()}>Create Posting</Button>
+      </Stack>
+    </Layout>
   );
 };
 
