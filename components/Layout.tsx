04da0b30d05862a210fdd7170e7df6c5c652dc2b
Nao k <light.kart92@gmail.com>
2023-08-28 09:33:38 +0900

    a few changes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,9 @@ export const Layout: FC<Props> = ({ children, title = '' }) => {
         <title>{title}</title>
       </Head>
       <WithSubnavigation />
-      <main>{children}</main>
+      <Box bgGradient='linear(to-l,rgba(7,27,82,1) 0%, rgba(0,128,128,1) 100%)'>
+        {children}
+      </Box>
     </Box>
   );
 };
