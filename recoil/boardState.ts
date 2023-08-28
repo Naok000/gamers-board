8a6bee51fd07f91497f32e27e1ba70927c940a5c
Nao k <light.kart92@gmail.com>
2023-08-28 09:42:54 +0900

    slight changes

--- recoil/boardState.ts
+++ recoil/boardState.ts
@@ -2,14 +2,10 @@ import { atom } from 'recoil';
 
 export type SessionUser = {
   id: string;
+  role: string;
 };
 
 export const currentUserId = atom<SessionUser | undefined>({
   key: 'currentUserId',
   default: undefined,
-});
-
-export const isLoginState = atom<boolean>({
-  key: 'isLogin',
-  default: false,
 });
